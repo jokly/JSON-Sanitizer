f15d0e4213c0392f2b4de0b199841a2489663df7
Slasten Tikhon <slasten32@gmail.com>
2018-05-27 12:35:48 +1000

    Added code to exceptions

--- src/SanitizerException.php
+++ src/SanitizerException.php
@@ -9,25 +9,25 @@
 
     class InvalidJsonException extends SanitizerException {
         function __construct($code = 0, \Exception $previous = null) {
-            parent::__construct('Invalid json object', $code, $previous);
+            parent::__construct('Invalid json object', 1, $previous);
         }
     }
 
     class UndefinedIndexException extends SanitizerException {
         function __construct($index, $code = 0, \Exception $previous = null) {
-            parent::__construct("Undefinde index: '$index'", $code, $previous);
+            parent::__construct("Undefinde index: '$index'", 2, $previous);
         }
     }
 
     class UnknownTypeException extends SanitizerException {
         function __construct($type, $code = 0, \Exception $previous = null) {
-            parent::__construct("Unknown type: '$type'", $code, $previous);
+            parent::__construct("Unknown type: '$type'", 3, $previous);
         }
     }
 
     class InvalidIntException extends SanitizerException {
         function __construct($data, $code = 0, \Exception $previous = null) {
-            parent::__construct("Invalid integer: '$data'", $code, $previous);
+            parent::__construct("Invalid integer: '$data'", 4, $previous);
         }
     }
 ?>
