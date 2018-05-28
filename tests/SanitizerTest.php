85c00faa207666f51fdea3fed7cdb75343286805
Slasten Tikhon <slasten32@gmail.com>
2018-05-28 15:09:25 +1000

    Added float tests

--- tests/SanitizerTest.php
+++ tests/SanitizerTest.php
@@ -13,8 +13,9 @@
 
         /**
          * @dataProvider int_provider
+         * @dataProvider float_provider
          */
-        public function test_simple_object(string $type, string $data, int $res_data) {
+        public function test_simple_object(string $type, string $data, $res_data) {
             $obj = [
                 [
                     'data' => "$data",
@@ -26,7 +27,7 @@
             $this->assertTrue($res);
 
             $res_arr = $this->sanitizer->get_sanitized_object();
-            $this->assertSame($res_arr[0], $res_data);
+            $this->assertEquals($res_arr[0], $res_data);
         }
 
         public function int_provider() {
@@ -37,6 +38,17 @@
                 ['int', '0032', 32],
                 ['int', '+0050', 50],
                 ['int', '-0830', -830],
+            ];
+        }
+
+        public function float_provider() {
+            return [
+                ['float', '-5', -5],
+                ['float', '3.01000', 3.01],
+                ['float', '+2.0031', 2.0031],
+                ['float', '-9.78', -9.78],
+                ['float', '-0005.9', -5.9],
+                ['float', '-32.', -32],
             ];
         }
     }
